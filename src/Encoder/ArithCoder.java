8bede041013c724603f7666ec556e47f8b7906bd
ChispyDev <102364981+ChispyDev@users.noreply.github.com>
2024-02-20 19:23:49 +0100

    Ranges set up method

--- src/Encoder/ArithCoder.java
+++ src/Encoder/ArithCoder.java
@@ -1,13 +1,29 @@
 package Encoder;
 
 import Alphabet.Alphabet;
+import utilities.Fraction;
 
 import java.util.ArrayList;
 
 public class ArithCoder {
     private Alphabet _alphabet;
+    private ArrayList<Fraction> lowRanges;
+    private ArrayList<Fraction> hightRanges;
 
     public ArithCoder(Alphabet alphabet){
         _alphabet = alphabet;
+        lowRanges = new ArrayList<>();
+        hightRanges = new ArrayList<>();
+        setUpRanges();
+    }
+    private void setUpRanges(){
+        long currentValue = 0;
+        for(int i = 0; i < _alphabet.size(); i++){
+            Fraction lowTemp = new Fraction(currentValue,_alphabet.getElem(i).getFracBot());
+            lowRanges.add(lowTemp);
+            currentValue += _alphabet.getElem(i).getFracTop();
+            Fraction hightTemp = new Fraction(currentValue,_alphabet.getElem(i).getFracBot());
+            hightRanges.add(hightTemp);
+        }
     }
 }
