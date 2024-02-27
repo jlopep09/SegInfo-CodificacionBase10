0ce2be3ba29c4594f1a20403e0375c4f8c41441a
Jose Antonio Lopez Perez <jose.lppz03@gmail.com>
2024-02-27 11:39:45 +0100

    Added new examples

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,27 @@
 import Alphabet.Alphabet;
 import Encoder.ArithCoder;
+import utilities.Fraction;
 
 import java.util.ArrayList;
 
 public class Main {
     public static void main(String args[]){
+
         System.out.println("---- Arithmetic coding system base 10 ----");
 
+        ejemplo2();
+
+
+
+
+
+    }
+    private static void ejemplo1(){
+        //creamos array de elementos y probabilidades relacionadas por su posicion en el arraylist
         ArrayList<String> elements = new ArrayList<>();
         ArrayList<Long> proportionList = new ArrayList<>();
 
+        //Añadimos los distintos elementos deseados a esas listas
         elements.add("A");
         proportionList.add((long)2);
 
@@ -19,20 +31,52 @@ public class Main {
         elements.add("C");
         proportionList.add((long)1);
 
+        //creamos el alphabet para esa lista de elementos y probabilidades
         Alphabet alphabet = new Alphabet(3);
         alphabet.setAlphabetElements(elements);
         alphabet.setAlphabetProportion(proportionList);
 
+        //creamos arithCoder, el constructor crea automaticamente las cotas iniciales
         ArithCoder arithCoder = new ArithCoder(alphabet);
         System.out.println(arithCoder.toString());
 
         System.out.println(arithCoder.encode("A"));
-        System.out.println(arithCoder.encode("A"));
-        System.out.println(arithCoder.encode("AB"));
-        System.out.println(arithCoder.encode("AB"));
-        System.out.println(arithCoder.encode("ABC"));
-        System.out.println(arithCoder.encode("ABC"));
 
+        System.out.println(arithCoder.encode("AB"));
 
+        System.out.println(arithCoder.encode("AAA"));
+        System.out.println(arithCoder.encode("ABB"));
+        System.out.println(arithCoder.encode("ABC"));
+        System.out.println(arithCoder.encode("AACCCCCC"));
+
+    }
+    private static void ejemplo2(){
+
+        //creamos array de elementos y probabilidades relacionadas por su posicion en el arraylist
+        ArrayList<String> elements = new ArrayList<>();
+        ArrayList<Long> proportionList = new ArrayList<>();
+
+        //Añadimos los distintos elementos deseados a esas listas
+        elements.add("P");
+        proportionList.add((long)1);
+        elements.add("A");
+        proportionList.add((long)2);
+        elements.add("S");
+        proportionList.add((long)1);
+        elements.add("E");
+        proportionList.add((long)2);
+        elements.add("L");
+        proportionList.add((long)1);
+
+        //creamos el alphabet para esa lista de elementos y probabilidades
+        Alphabet alphabet = new Alphabet(5);
+        alphabet.setAlphabetElements(elements);
+        alphabet.setAlphabetProportion(proportionList);
+
+        //creamos arithCoder, el constructor crea automaticamente las cotas iniciales
+        ArithCoder arithCoder = new ArithCoder(alphabet);
+        System.out.println(arithCoder.toString());
+
+        System.out.println(arithCoder.encode("ASA"));
     }
 }
